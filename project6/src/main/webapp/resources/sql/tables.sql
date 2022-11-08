8c2910596831fc3c981effe6186dbb73a5a340f9
devbeta8t3 <sehoan30@gmail.com>
2022-11-08 11:16:02 +0900

    SQL FK cascade option updated

--- project6/src/main/webapp/resources/sql/tables.sql
+++ project6/src/main/webapp/resources/sql/tables.sql
@@ -1,4 +1,4 @@
--- users
+-- users 사용자 테이블
 CREATE TABLE USERS(
    U_ID varchar2(20) primary key,
    U_NAME varchar2(20) NOT NULL,
@@ -7,14 +7,15 @@ CREATE TABLE USERS(
    U_REGDATE date default sysdate
  );
 
--- favorites
+-- favorites 즐겨찾기 테이블
 create table cryptoFavorite (
 	u_id varchar2(20) not null,
 	symbol varchar2(10) not null
 );
-alter table cryptoFavorite ADD CONSTRAINT fk_fav foreign KEY(u_id) references users (u_id);
-drop table cryptoFavorite;
+-- FK 설정하기 (종속 삭제)
+alter table cryptoFavorite ADD CONSTRAINT fk_fav foreign KEY(u_id) references users (u_id) ON DELETE CASCADE ENABLE;
 
+-- 즐겨찾기 테스트 데이터 삽입
 insert into cryptoFavorite values ('aaa', 'BTC');
 insert into cryptoFavorite values ('aaa', 'ADA');
 insert into cryptoFavorite values ('aaa', 'ETH');
@@ -24,3 +25,5 @@ insert into cryptoFavorite values ('aaa', 'TRX');
 insert into cryptoFavorite values ('aaa', 'FLOW');
 
 select u_id, symbol from cryptoFavorite where u_id = 'aaa';
+
+drop table cryptoFavorite;
